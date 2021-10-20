772b059f4e52509d92f71d3f2a84bec5fb5b7694
Pelle <pelleseshibe@outlook.com>
2021-10-21 00:07:33 +0200

    built service registration

---
+++ MessageProducer/MessageProducer/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageProducer
+{
+   public class ServiceRegistration
+    {
+        public IServiceProvider Build()
+        {
+            var container = new ServiceCollection();
+            container.AddScoped<IProduceMessageService, ProduceMessageService>();
+            container.AddScoped<IMessagerChannel, MessagerChannel>();
+            return container.BuildServiceProvider();
+        }
+    }
+}
