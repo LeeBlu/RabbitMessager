43e7853eff7b2b07288df6bddededd6d8a1e6cd9
Pelle <pelleseshibe@outlook.com>
2021-10-21 01:17:19 +0200

    implemented ServiceRegistration

---
+++ MessageReceiver/MessageReceiver/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageReceiver
+{
+    public class ServiceRegistration
+    {
+        public IServiceProvider Build()
+        {
+            var container = new ServiceCollection();
+            container.AddScoped<IReceiveMessageService, ReceiveMessageService>();
+            container.AddScoped<IMessagerChannel, MessagerChannel>();
+            return container.BuildServiceProvider();
+        }
+    }
+}
