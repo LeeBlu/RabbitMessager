7fb384d8d3f0caab77d9a3014ef64461fb06a5bf
Pelle <pelleseshibe@outlook.com>
2021-10-21 00:34:59 +0200

    implemented service call on main function

--- MessageProducer/MessageProducer/Program.cs
+++ MessageProducer/MessageProducer/Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MessageProducer
 {
@@ -9,7 +10,15 @@ namespace MessageProducer
     {
         static void Main(string[] args)
         {
+            IServiceProvider service = new ServiceRegistration().Build();
+
+            var _produceMessage = service.GetService<IProduceMessageService>();
   
+            string name = "";
+            Console.WriteLine("enter name ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello my name is,{0}", name);
+            _produceMessage.SendMessage(name);
         }
     }
 }
